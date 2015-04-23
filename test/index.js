9413446b97041537c61895910ddd1d50e45f95c1
Jan Krems <jan.krems@groupon.com>
2015-04-22 21:44:44 -0700

    Remove noop line

--- test/index.js
+++ test/index.js
@@ -3,8 +3,6 @@
 const test = require('tape');
 const path = require('path');
 
-test.createStream().pipe(process.stdout);
-
 process.argv.slice(2).forEach(file => {
   require(path.resolve(file));
 });
